9987fa332c22cbeaa0af094fabb338309cae40a6
Marton Cserbak <cm@onskeskyen.dk>
2023-12-01 06:37:28 +0100

    chore: Fix boilerplate

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,19 @@
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
     "env": {
         "browser": true,
         "es2021": true
     },
-    "extends": "standard-with-typescript",
+    // "extends": "standard-with-typescript",
     "overrides": [
     ],
     "parserOptions": {
         "ecmaVersion": "latest",
-        "sourceType": "module"
+        "sourceType": "module",
     },
+    "plugins": ["@stylistic"],
     "rules": {
+        "@stylistic/quotes": ["error", "double", { "avoidEscape": true }],
+        "@stylistic/semi": "error"
     }
-}
+};
