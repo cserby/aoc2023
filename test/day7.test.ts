3fedfd6be5ef9cf730e171cdcc315bfe8f63e65f
Marton Cserbak <cm@onskeskyen.dk>
2023-12-07 14:32:05 +0100

    feat: Day7 part2

--- test/day7.test.ts
+++ test/day7.test.ts
@@ -1,4 +1,4 @@
-import { compareHands, day7part1, parseHand, parseInput } from "../src/day7";
+import { compareHands, day7part1, day7part2, fourOfAKind, fullHouse, parseHand, parseInput, threeOfAKind, twoPairs } from "../src/day7";
 import { readFileSync } from "fs";
 
 const sample = `32T3K 765
@@ -64,6 +64,46 @@ describe("Day7", () => {
 
     test("Real", () => {
       expect(day7part1(readFileSync("inputs/day7.txt", { encoding: "utf-8" }))).toEqual(250232501);
+    });
+  });
+
+  describe("Part2", () => {
+    test("Two pairs", () => {
+      expect(twoPairs(parseHand("AAKK2"), true)).toBe(true);
+      expect(twoPairs(parseHand("AAKJ2"), true)).toBe(true);
+      expect(twoPairs(parseHand("AJKJ2"), true)).toBe(true);
+      expect(twoPairs(parseHand("AJKT2"), true)).toBe(false);
+    });
+
+    test("Full house", () => {
+      expect(fullHouse(parseHand("AAKKK"), true)).toBe(true);
+      expect(fullHouse(parseHand("AAKKJ"), true)).toBe(true);
+      expect(fullHouse(parseHand("AJKKJ"), true)).toBe(true);
+      expect(fullHouse(parseHand("ATKKJ"), true)).toBe(false);
+    });
+
+    test("Four of a kind", () => {
+      expect(fourOfAKind(parseHand("QJJQ2"), true)).toBe(true);
+    });
+
+    test("Three of a kind", () => {
+      expect(threeOfAKind(parseHand("33354"), true)).toBe(true);
+    });
+
+    test("Compare", () => {
+      expect([parseHand("QJJQ2"), parseHand("QQQQ2")].sort(compareHands))
+        .toEqual([
+          expect.objectContaining({ cards: [..."QJJQ2"]}),
+          expect.objectContaining({ cards: [..."QQQQ2"]}),
+        ]);
+    });
+
+    test("Sample", () => {
+      expect(day7part2(sample)).toEqual(5905);
+    });
+
+    test("Real", () => {
+      expect(day7part2(readFileSync("inputs/day7.txt", { encoding: "utf-8" }))).toEqual(249138943);
     });
   });
 });
