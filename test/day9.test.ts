db7da51ee15942121de5c65f8973526d97ba1e35
Marton Cserbak <cm@onskeskyen.dk>
2023-12-09 09:38:42 +0100

    feat: Day9 part1

---
+++ test/day9.test.ts
@@ -0,0 +1,22 @@
+import { day9part1 } from "../src/day9";
+import { readFileSync } from "fs";
+
+const sample = `0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45`;
+
+describe("Day9", () => {
+  describe("Part1", () => {
+    test("Sample0", () => {
+      expect(day9part1(sample.split("\n")[0])).toEqual(18);
+    });
+
+    test("Sample", () => {
+      expect(day9part1(sample)).toEqual(114);
+    });
+
+    test("Real", () => {
+      expect(day9part1(readFileSync("inputs/day9.txt", { encoding: "utf-8" }))).toEqual(2098530125);
+    });
+  });
+});
