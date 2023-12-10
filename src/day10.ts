a9adf114e271a8fedddf0622497980e01f5da609
Marton Cserbak <cm@onskeskyen.dk>
2023-12-10 12:15:48 +0100

    feat: Day10 part1

---
+++ src/day10.ts
@@ -0,0 +1,129 @@
+type Cell = "|" | "-" | "L" | "J" | "7" | "F" | "." | "S";
+type Direction = "S" | "N" | "E" | "W";
+
+export interface Coords {
+  line: number;
+  char: number;
+}
+
+export interface Map {
+  start: Coords;
+  lines: Cell[][];
+}
+
+export function parseInput(input: string): Map {
+  let start: Coords = { line: -1, char: -1 };
+  
+  const lines = input.split("\n").map((line, lineNum) => [...line].map((char, charNum) => {
+    const charAsCell = char as Cell;
+    if (charAsCell === "S") {
+      start = { line: lineNum, char: charNum };
+    }
+    return charAsCell;
+  }));
+  
+  return { start, lines };
+}
+
+type Distances = Array<number | undefined>[];
+
+export interface State {
+  pos: Coords;
+  heading: Direction;
+  distance: number;
+}
+
+function nextPos(pos: Coords, heading: Direction, map: Map): Coords | undefined {
+  switch (heading) {
+    case "S": return pos.line < map.lines.length - 1 ? { line: pos.line + 1, char: pos.char } : undefined;
+    case "N": return pos.line > 0 ? { line: pos.line - 1, char: pos.char } : undefined;
+    case "E": return pos.char < map.lines[0].length - 1 ? { line: pos.line, char: pos.char + 1 } : undefined;
+    case "W": return pos.char > 0 ? { line: pos.line, char: pos.char - 1 } : undefined;
+  }
+}
+
+function nextHeading(heading: Direction, pos: Coords, map: Map): Direction | undefined {
+  const cell = map.lines[pos.line][pos.char];
+
+  switch (heading) {
+    case "N": {
+      switch (cell) {
+        case "7": return "W";
+        case "F": return "E";
+        case "|": return "N";
+        default: return;
+      };
+    };
+    case "S": {
+      switch (cell) {
+        case "J": return "W";
+        case "L": return "E";
+        case "|": return "S";
+        default: return;
+      };
+    };
+    case "E": {
+      switch (cell) {
+        case "-": return "E";
+        case "7": return "S";
+        case "J": return "N";
+        default: return;
+      };
+    };
+    case "W": {
+      switch (cell) {
+        case "-": return "W";
+        case "F": return "S";
+        case "L": return "N";
+        default: return;
+      };
+    };
+  }
+}
+
+function nextState(state: State, map: Map): State | undefined {
+  const nPos = nextPos(state.pos, state.heading, map);
+
+  if (nPos === undefined) return;
+
+  const nHeading = nextHeading(state.heading, nPos, map);
+
+  if (nHeading === undefined) return;
+
+  return { pos: nPos, heading: nHeading, distance: state.distance + 1 };
+}
+
+export function findDistances(map: Map): Distances {
+  const distances = Array(map.lines.length).fill(undefined).map(() => Array(map.lines[0].length).fill(Infinity));
+
+  distances[map.start.line][map.start.char] = 0;
+
+  const needToVisit: State[] = [
+    nextState({pos: map.start, heading: "E", distance: 0}, map),
+    nextState({pos: map.start, heading: "W", distance: 0}, map),
+    nextState({pos: map.start, heading: "S", distance: 0}, map),
+    nextState({pos: map.start, heading: "N", distance: 0}, map),
+  ].filter((s) => s !== undefined) as State[];
+
+  while (needToVisit.length > 0) {
+    const state = needToVisit.pop() as State;
+    
+    if (distances[state.pos.line][state.pos.char] <= state.distance) {
+      continue;
+    }
+
+    distances[state.pos.line][state.pos.char] = state.distance;
+
+    const nst = nextState(state, map);
+
+    if (nst !== undefined) {
+      needToVisit.push(nst);
+    }
+  }
+
+  return distances;
+}
+
+export function day10part1(input: string): number {
+  return Math.max(...(findDistances(parseInput(input)).flatMap((l) => l.flatMap(c => c)).filter((c) => c!== Infinity)) as number[]);
+}
---
+++ test/day10.test.ts
@@ -0,0 +1,59 @@
+import { readFileSync } from "fs";
+import { day10part1, findDistances, parseInput } from "../src/day10";
+
+const sample1 = `-L|F7
+7S-7|
+L|7||
+-L-J|
+L|-JF`;
+
+const sample2 = `7-F7-
+.FJ|7
+SJLL7
+|F--J
+LJ.LJ`;
+
+describe("Day10", () => {
+  describe("Part1", () => {
+    test("Parsing", () => {
+      expect(parseInput(sample1)).toEqual(expect.objectContaining({
+        start: { line: 1, char: 1 },
+      }));
+      expect(parseInput(sample2)).toEqual(expect.objectContaining({
+        start: { line: 2, char: 0 },
+      }));
+    });
+
+    test("FindDistances", () => {
+      expect(findDistances(parseInput(sample1))).toEqual([
+        [Infinity, Infinity, Infinity, Infinity, Infinity],
+        [Infinity, 0,        1,        2,        Infinity],
+        [Infinity, 1,        Infinity, 3,        Infinity],
+        [Infinity, 2,        3,        4,        Infinity],
+        [Infinity, Infinity, Infinity, Infinity, Infinity],
+      ]);
+
+      expect(findDistances(parseInput(sample2))).toEqual(
+        [
+          [Infinity, Infinity, 4,        5,        Infinity],
+          [Infinity, 2,        3,        6,        Infinity],
+          [0,        1,        Infinity, 7,        8       ],
+          [1,        4,        5,        6,        7       ],
+          [2,        3,        Infinity, Infinity, Infinity],
+        ],
+      );
+    });
+  });
+
+  test("Sample1", () => {
+    expect(day10part1(sample1)).toEqual(4);
+  });
+
+  test("Sample2", () => {
+    expect(day10part1(sample2)).toEqual(8);
+  });
+
+  test("Real", () => {
+    expect(day10part1(readFileSync("inputs/day10.txt", { encoding: "utf-8" }))).toEqual(6979);
+  });
+});
