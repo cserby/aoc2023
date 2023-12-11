e22dd421512f64b0b1c005e6f5e9336c07f381f2
Marton Cserbak <cm@onskeskyen.dk>
2023-12-11 09:07:20 +0100

    feat: Day11 part1

---
+++ test/day11.test.ts
@@ -0,0 +1,58 @@
+import { readFileSync } from "fs";
+import { day11part1, distance, expand, parseInput } from "../src/day11";
+
+const sample = `...#......
+.......#..
+#.........
+..........
+......#...
+.#........
+.........#
+..........
+.......#..
+#...#.....`;
+
+describe("Day11", () => {
+  describe("Part1", () => {
+    test("Parsing", () => {
+      expect(parseInput(sample)).toEqual(expect.objectContaining({
+        stars: expect.arrayContaining([
+          { line: 0, char: 3 },
+          { line: 2, char: 0 },
+          { line: 6, char: 9 },
+        ]),
+        emptyLines: [3, 7],
+        emptyChars: [2, 5, 8],
+      }))
+    });
+
+    test("Expand", () => {
+      expect(expand(parseInput(sample))).toEqual(parseInput(`....#........
+.........#...
+#............
+.............
+.............
+........#....
+.#...........
+............#
+.............
+.............
+.........#...
+#....#.......`));
+    });
+
+    test("Distance", () => {
+      expect(distance({ line: 6, char: 1 }, { line: 11, char: 5 })).toEqual(9);
+
+      expect(distance({ line: 6, char: 1 }, { line: 11, char: 1 })).toEqual(5);
+    });
+
+    test("Sample", () => {
+      expect(day11part1(sample)).toEqual(374);
+    });
+
+    test("Real", () => {
+      expect(day11part1(readFileSync("inputs/day11.txt", { encoding: "utf-8" }))).toEqual(10494813);
+    });
+  });
+});
