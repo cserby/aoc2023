c2a5dc3a1a4bb6f503c1f7468248c3e69a6267a4
Marton Cserbak <cm@onskeskyen.dk>
2023-12-12 18:45:03 +0100

    feat: Day12 part2 - naive

--- test/day12.test.ts
+++ test/day12.test.ts
@@ -1,5 +1,5 @@
 import { readFileSync } from "fs";
-import { day12part1, parseInput, possibleSolutions, runLengths } from "../src/day12";
+import { day12part1, day12part2, parseInput, possibleSolutions, runLengths, unfold } from "../src/day12";
 
 const sample = `???.### 1,1,3
 .??..??...?##. 1,1,3
@@ -40,5 +40,41 @@ describe("Day12", () => {
     test("Real", () => {
       expect(day12part1(readFileSync("inputs/day12.txt", { encoding: "utf-8" }))).toEqual(6852);
     });
+  });
+
+  describe("Part2", () => {
+    test("Unfold", () => {
+      expect(unfold(parseInput(sample)[0])).toEqual({
+        fields: [..."???.###????.###????.###????.###????.###"],
+        runLengths: [1, 1, 3, 1, 1, 3, 1, 1, 3, 1, 1, 3, 1, 1, 3],
+      });
+    });
+
+    test("Sample1", () => {
+      expect(day12part2(sample.split("\n")[0])).toEqual(1);
+    });
+
+    test("Sample2", () => {
+      // v8Profiler.setGenerateType(1);
+      // v8Profiler.startProfiling("Sample2", true);
+      expect(day12part2(sample.split("\n")[1])).toEqual(16384);
+      // const profile = v8Profiler.stopProfiling("Sample2");
+      // profile.export((error, result) => {
+      //   writeFileSync("Sample2.cpuprofile", result ?? "");
+      //   profile.delete();
+      // });
+    });
+
+    test("Sample3", () => {
+      expect(day12part2(sample.split("\n")[2])).toEqual(1);
+    });
+
+    test("Sample from real", () => {
+      expect(day12part2("???#???.?#?????? 1,2,2,3,2")).toEqual(1);
+    });
+
+    // test("Real", () => {
+    //   expect(day12part2(readFileSync("inputs/day12.txt", { encoding: "utf-8" }))).toEqual(-1);
+    // });
   });
 });
