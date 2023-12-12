26ee74c1372322af14aa7fa8aceb90443f5052fa
Marton Cserbak <cm@onskeskyen.dk>
2023-12-12 18:45:03 +0100

    feat: Day12 part2 - naive

--- src/day12.ts
+++ src/day12.ts
@@ -17,45 +17,57 @@ export function parseInput(input: string): Arrangement[] {
 }
 
 export function runLengths(fields: string[]): number[] {
-  function rlrec(fieldsLeft: string[], inRun: boolean, currRunLen: number, prevRunLens: number[]): number[] {
-    const [head, ...tail] = fieldsLeft;
+  return fields.join("").split(/[.?]/).map((s) => s.length).filter((l) => l !== 0);
+}
 
-    if (head === undefined) { // end of string
-      if (inRun) {
-        return [...prevRunLens, currRunLen];
-      } else {
-        return prevRunLens;
-      }
-    } else if (head === "#") {
-      if (inRun) {
-        return rlrec(tail, true, currRunLen + 1, prevRunLens);
-      } else {
-        return rlrec(tail, true, 1, prevRunLens);
-      }
-    } else { // "?" or "."
-      if (inRun) {
-        return rlrec(tail, false, 0, [...prevRunLens, currRunLen]);
-      } else {
-        return rlrec(tail, false, 0, prevRunLens);
-      }
+function arrayStartsWith<T>(compareTo: T[], compared: T[]): boolean {
+  for (let i = 0; i < compared.length; i++) {
+    if (compareTo[i] !== compared[i]) {
+      return false;
     }
   }
 
-  return rlrec(fields, false, 0, []);
+  return true;
 }
 
-function isSolution(fields: string[], arr: Arrangement): boolean {
-  return fields.find((f) => f === "?") !== undefined || JSON.stringify(runLengths(fields)) === JSON.stringify(arr.runLengths);
+function canBeSolution(fields: string[], arr: Arrangement): boolean {
+  const sureRunLengths = runLengths(fields);
+  if (sureRunLengths.length === 0) {
+    return true;
+  } else if (sureRunLengths.length === 1) {
+    return sureRunLengths[0] <= arr.runLengths[0];
+  } else {
+    return arrayStartsWith(arr.runLengths, sureRunLengths.slice(undefined, -1)) &&
+      sureRunLengths[sureRunLengths.length - 1] <= arr.runLengths[sureRunLengths.length - 1] &&
+      arr.runLengths.slice(sureRunLengths.length).reduce((a, b) => a + b, 0) <= arr.fields.slice(fields.length).filter((f) => f === "?" || f === "#").length;
+  }
+}
+
+function isASolution(fields: string[], arr: Arrangement): boolean {
+  return JSON.stringify(runLengths(fields)) === JSON.stringify(arr.runLengths);
+}
+
+function takeWhileSure(fields: string[]): [string[], string[]] {
+  const unsureIndex = fields.indexOf("?");
+
+  if (unsureIndex === -1) {
+    return [fields, []];
+  } else {
+    return [fields.slice(undefined, unsureIndex), fields.slice(unsureIndex, undefined)];
+  }
 }
 
 export function possibleSolutions(arr: Arrangement): string[] {
   function psrec(fields: string[], decidedFields: string[]): string[] {
     const [head, ...tail] = fields;
 
-    if (head === undefined) {
-      return isSolution(decidedFields, arr) ? [decidedFields.join("")] : [];
+    if (!canBeSolution(decidedFields, arr)) {
+      return [];
+    } else if (head === undefined) {
+      return isASolution(decidedFields, arr) ? [decidedFields.join("")] : [];
     } else if (head === "." || head === "#") {
-      return psrec(tail, [...decidedFields, head]);
+      const [sure, unsure] = takeWhileSure(fields);
+      return psrec(unsure, [...decidedFields, ...sure]);
     } else if (head === "?") {
       return [
         ...psrec(tail, [...decidedFields, "."]),
@@ -71,4 +83,35 @@ export function possibleSolutions(arr: Arrangement): string[] {
 
 export function day12part1(input: string): number {
   return parseInput(input).map(possibleSolutions).map((ps) => ps.length).reduce((a, b) => a + b);
+}
+
+export function unfold(arr: Arrangement): Arrangement {
+  return {
+    fields: [
+      ...arr.fields,
+      "?",
+      ...arr.fields,
+      "?",
+      ...arr.fields,
+      "?",
+      ...arr.fields,
+      "?",
+      ...arr.fields,
+    ],
+    runLengths: [
+      ...arr.runLengths,
+      ...arr.runLengths,
+      ...arr.runLengths,
+      ...arr.runLengths,
+      ...arr.runLengths,
+    ],
+  };
+}
+
+export function day12part2(input: string): number {
+  return parseInput(input)
+    .map(unfold)
+    .map(possibleSolutions)
+    .map((ps) => ps.length)
+    .reduce((a, b) => a + b);
 }
