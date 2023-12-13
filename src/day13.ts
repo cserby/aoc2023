727316371c9ec97a2bf7df1a8a2a2dd36be9448b
Marton Cserbak <cm@onskeskyen.dk>
2023-12-13 11:31:20 +0100

    feat: Day13 part2 (runs on sample, gives incorrect result)

--- src/day13.ts
+++ src/day13.ts
@@ -24,9 +24,30 @@ export function isMirrorAfterLine(puzzle: Puzzle, line: number): boolean {
   return true;
 }
 
-export function findHorizontalMirror(puzzle: Puzzle): number | undefined {
+export function lineDiff(line1: string[], line2: string[]): number {
+  return line1.reduce((errors, _, index) => 
+    errors + (line1[index] === line2[index] ? 0 : 1)
+  , 0);
+}
+
+export function isMirrorAfterLineWithSmudge(puzzle: Puzzle, line: number): boolean {
+  const maxDiff = Math.min(line, puzzle.lines.length - line - 2);
+  let errors = 0;
+  for (let i = 0; i <= maxDiff; i++) {
+    errors += lineDiff(puzzle.lines[line - i], puzzle.lines[line + i + 1]);
+    if (errors > 1) {
+      return false;
+    }
+  }
+  return true;
+}
+
+export function findHorizontalMirror(puzzle: Puzzle, allowSmudge = false): number | undefined {
   for (let line = 0; line < puzzle.lines.length - 1; line++) {
-    if (isMirrorAfterLine(puzzle, line)) {
+    if (
+      (!allowSmudge && isMirrorAfterLine(puzzle, line)) ||
+      (allowSmudge && isMirrorAfterLineWithSmudge(puzzle, line))
+    ) {
       return line + 1;
     }
   }
@@ -36,8 +57,23 @@ export function findHorizontalMirror(puzzle: Puzzle): number | undefined {
 export function day13part1(input: string): number {
   return parseInput(input).reduce((prevSum, puzzle) => {
     const horizontal = findHorizontalMirror(puzzle);
-    const vertical = findHorizontalMirror(transpose(puzzle));
-//     return prevSum + 100 * (horizontal ?? 0) + (vertical ?? 0);
+    if (horizontal !== undefined) {
+      return prevSum + 100 * horizontal;
+    } else {
+      const vertical = findHorizontalMirror(transpose(puzzle));
+      if (vertical !== undefined) {
+        return prevSum + vertical;
+      } else {
+        throw new Error("No mirror found");
+      }
+    }
+  }, 0);
+}
+
+export function day13part2(input: string): number {
+  return parseInput(input).reduce((prevSum, puzzle) => {
+    const horizontal = findHorizontalMirror(puzzle, true);
+    const vertical = findHorizontalMirror(transpose(puzzle), true);
     if (horizontal !== undefined) {
       return prevSum + 100 * horizontal;
     } else {
