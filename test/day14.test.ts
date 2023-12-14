a69fe908939b3ac68dff055c5d6629ba80129883
Marton Cserbak <cm@onskeskyen.dk>
2023-12-14 16:31:38 +0100

    feat: Functional day14 part2, OOM

--- test/day14.test.ts
+++ test/day14.test.ts
@@ -1,5 +1,6 @@
 import { readFileSync } from "fs";
-import { day14part1 } from "../src/day14";
+import { cycle, day14part1, day14part2, parseInput, rotateLeft, rotateRight, tiltLeft, transpose } from "../src/day14";
+import { error } from "console";
 
 const sample = `O....#....
 O.OO#....#
@@ -21,5 +22,61 @@ describe("Day14", () => {
     test("Real", () => {
       expect(day14part1(readFileSync("inputs/day14.txt", { encoding: "utf-8" }))).toEqual(108759);
     });
+  });
+
+  describe("Part2", () => {
+    test("Tilt left", () => {
+      expect(tiltLeft([[...".#.O.O..#.O"]])).toEqual([[
+        ...".#OO....#O."
+      ]]);
+    });
+
+    test("Sample cycles", () => {
+      expect(
+        rotateRight(cycle(rotateLeft(parseInput(sample))))).toEqual(
+          `.....#....
+....#...O#
+...OO##...
+.OO#......
+.....OOO#.
+.O#...O#.#
+....O#....
+......OOOO
+#...O###..
+#..OO#....`.split("\n").map((l) => [...l])
+        );
+      
+      expect(
+        rotateRight(cycle(cycle(rotateLeft(parseInput(sample)))))).toEqual(
+          `.....#....
+....#...O#
+.....##...
+..O#......
+.....OOO#.
+.O#...O#.#
+....O#...O
+.......OOO
+#..OO###..
+#.OOO#...O`.split("\n").map((l) => [...l])
+        );
+
+      expect(
+        rotateRight(cycle(cycle(cycle(rotateLeft(parseInput(sample))))))).toEqual(
+          `.....#....
+....#...O#
+.....##...
+..O#......
+.....OOO#.
+.O#...O#.#
+....O#...O
+.......OOO
+#...O###.O
+#.OOO#...O`.split("\n").map((l) => [...l])
+        );
+    });
+
+    // test("Sample calc", () => {
+    //   expect(day14part2(sample)).toEqual(64);
+    // });
   });
 });
