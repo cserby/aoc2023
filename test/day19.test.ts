87db5c7160b2ddcb87fd964c5cb7317cce408fc0
Marton Cserbak <cm@onskeskyen.dk>
2023-12-19 07:24:38 +0100

    feat: Day19 part1

---
+++ test/day19.test.ts
@@ -0,0 +1,32 @@
+import { readFileSync } from "fs";
+import { day19part1 } from "../src/day19";
+
+const sample = `px{a<2006:qkq,m>2090:A,rfg}
+pv{a>1716:R,A}
+lnx{m>1548:A,A}
+rfg{s<537:gd,x>2440:R,A}
+qs{s>3448:A,lnx}
+qkq{x<1416:A,crn}
+crn{x>2662:A,R}
+in{s<1351:px,qqz}
+qqz{s>2770:qs,m<1801:hdj,R}
+gd{a>3333:R,R}
+hdj{m>838:A,pv}
+
+{x=787,m=2655,a=1222,s=2876}
+{x=1679,m=44,a=2067,s=496}
+{x=2036,m=264,a=79,s=2244}
+{x=2461,m=1339,a=466,s=291}
+{x=2127,m=1623,a=2188,s=1013}`;
+
+describe("Day19", () => {
+  describe("Part1", () => {
+    test("Sample", () => {
+      expect(day19part1(sample)).toEqual(19114);
+    });
+
+    test("Real", () => {
+      expect(day19part1(readFileSync("inputs/day19.txt", { encoding: "utf-8" }))).toEqual(456651);
+    });
+  });
+});
